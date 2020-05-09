a96d8ea60ce0165d21719d8a31b3be755e04f4b0
Leonid Petrov <lp.msarchitect@gmail.com>
2020-05-09 21:13:12 +0300

    feat: promise handlers

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,8 +1,10 @@
-import { Controller } from 'swiper/js/swiper.esm';
+import Controller from './js/Controller';
 import Model from './js/Model';
 import View from './js/View';
 
 const model = new Model();
-const view = new View('app');
+const view = new View('app', document);
 const controller = new Controller(model, view);
-controller.init();
+controller.init().then(() => {
+  controller.addHandlers();
+});
