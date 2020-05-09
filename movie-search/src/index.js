41018a0cfd40bde1d2d34957d7c05bea59e70b71
Leonid Petrov <lp.msarchitect@gmail.com>
2020-05-09 22:09:49 +0300

    refactor: api keys to constant

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -2,7 +2,13 @@ import Controller from './js/Controller';
 import Model from './js/Model';
 import View from './js/View';
 
-const model = new Model();
+const API_KEYS = {
+  ytrnsl:
+    'trnsl.1.1.20200426T152125Z.cb620e66f793c290.b8233de83d42a48bd5547479b9a206f77b57ea96',
+  omdb: '7f33cc87',
+};
+
+const model = new Model(API_KEYS);
 const view = new View('app', document);
 const controller = new Controller(model, view);
 controller.init().then(() => {
--- movie-search/src/js/Model.js
+++ movie-search/src/js/Model.js
@@ -1,11 +1,11 @@
 export default class Model {
   constructor(apiKeys) {
     this.apiKeys = apiKeys;
-    // TODO Api keys
   }
 
   async getMovies(query, page = 1) {
     const data = await this.getData(query, page);
+    console.log(data);
 
     if (data.Response === 'True') {
       const moviesList = data.Search;
@@ -19,13 +19,13 @@ export default class Model {
   }
 
   async getData(string = '', page = 1) {
-    const url = `https://www.omdbapi.com/?s=${string}&page=${page}&apikey=7f33cc87`;
+    const url = `https://www.omdbapi.com/?s=${string}&page=${page}&apikey=${this.apiKeys.omdb}`;
     const res = await fetch(url);
     return await res.json();
   }
 
   async getTranslate(str) {
-    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200426T152125Z.cb620e66f793c290.b8233de83d42a48bd5547479b9a206f77b57ea96&text=${str}&lang=ru-en`;
+    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${this.apiKeys.ytrnsl}&text=${str}&lang=ru-en`;
 
     const res = await fetch(url);
     const json = await res.json();
@@ -35,9 +35,11 @@ export default class Model {
   async setMoviesRating(movies) {
     return await Promise.all(
       movies.map(async (movie) => {
-        const url = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=7f33cc87`;
+        const url = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${this.apiKeys.omdb}`;
         const res = await fetch(url);
         const json = await res.json();
+        console.log(json);
+
         movie.rating = json.imdbRating;
         return movie;
       })
@@ -68,24 +70,5 @@ export default class Model {
     const data = await this.getData(request);
 
     return Math.ceil(data.totalResults / 10);
-  }
-
-  async getMoviesPage(request = '', page = 1) {
-    this.currentPage = page;
-    const data = await this.getData(request, page);
-    console.log('response data: ', data);
-    return new Promise(function (resolve, reject) {
-      if (data.Response === 'True') {
-        resolve(data.Search);
-      } else {
-        reject(data.Error);
-      }
-    });
-  }
-
-  async getMovieRating(id) {
-    const res = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=7f33cc87`);
-    const data = await res.json();
-    return data.imdbRating;
   }
 }
